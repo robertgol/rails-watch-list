e6dd623db9668dc52dad49ebfb954a50d88ee9cf
Robert Golusinski <robertgolusinski@gmail.com>
2025-11-30 12:12:34 +0100

    added list name length validation

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: {maximum: 20}
 end
