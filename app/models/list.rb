d9c68e4c36a41bc0e2464dd0057553464609d7e1
Robert Golusinski <robertgolusinski@gmail.com>
2025-12-01 23:00:45 +0100

    add user to lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ApplicationRecord
+  belongs_to :user
+
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_01_203229) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_01_215558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_01_203229) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -55,4 +57,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_01_203229) do
 
   add_foreign_key "bookmarks", "lists"
   add_foreign_key "bookmarks", "movies"
+  add_foreign_key "lists", "users"
 end
